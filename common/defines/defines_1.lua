6fe476830a65cbbeff623e8598176181d9375adf
Jingo Spunk <30711461+JingoSpunk@users.noreply.github.com>
2021-04-16 00:23:44 -0700

    new gfx stuff

---
+++ common/defines/defines_1.lua
@@ -0,0 +1,16 @@
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {450, 500, 1000}
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {250, 350, 900}
+
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_HUE_MODIFIER = 0.0
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.8 -- 1.0 -- 0.7
+--NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.85 -- 0.75
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 5.0
+
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_NEAR = 350
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_FAR = 355
+--NDefines.NFrontend.CAMERA_MIN_HEIGHT = 25.0
+--NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3625.0 
+NDefines_Graphics.NGraphics.TOOLTIP_DELAYED_DELAY = 0 				--How long before showing delayed tooltip.
+NDefines_Graphics.NGraphics.TOOLTIP_SHOW_DELAY = 0 					--How long before showing delayed tooltip.
+NDefines_Graphics.NGraphics.TOOLTIP_HIDE_DELAY = 0 					--How long before showing delayed tooltip.
